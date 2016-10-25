34d4d5dae7100b397a0cf754439fc53fa766fdb0
Stephen Woodford <stephen.woodford@thefreeosk.com>
2016-10-25 13:00:38 -0500

    Add example Sleep Timer functionality

--- Sleep/ViewController.swift
+++ Sleep/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var sleepTimerButton: UIButton!
+    
+    let sleepTimerMinuteOptions = [15, 30, 45, 60, 90, 120]
+    
+    var sleepTimer: Timer? = nil
+    var sleepTimerButtonUpdateTimer: Timer? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,63 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sleepClick(_ sender: AnyObject) {
+        
+        let alert = UIAlertController(title: nil, message: "Setting the Sleep Timer will exit back to the Guide after the time has elapsed to allow your Apple TV's Sleep After setting to put your TV to sleep.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Off", style: .default, handler: { (action) in
+            self.cancelSleepTime()
+        }))
+        
+        for minuteOption in sleepTimerMinuteOptions {
+            alert.addAction(UIAlertAction(title: "\(minuteOption) minutes", style: .default, handler: { (action) in
+                self.setSleepTime(minuteOption)
+            }))
+        }
+        
+        alert.addAction(UIAlertAction(title: "Back", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func setSleepTime(_ minutes: Int) {
+        let sleepSeconds = TimeInterval(minutes * 60)
+        
+        cancelSleepTime()
+        
+        sleepTimer = Timer.scheduledTimer(timeInterval: sleepSeconds, target: self, selector: #selector(sleepFire), userInfo: nil, repeats: false)
+        
+        sleepTimerButtonUpdateTimer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(updateSleepTimerButton), userInfo: nil, repeats: true)
+        
+        sleepTimerButton.setTitle("Sleep Timer: \(minutes) min", for: .normal)
+    }
+    
+    func updateSleepTimerButton() {
+        let remainingMinutes = Calendar.current.dateComponents([.minute], from: Date.init(), to: (sleepTimer?.fireDate)!).minute ?? 0
+        
+        sleepTimerButton.setTitle("Sleep Timer: \(remainingMinutes) min", for: .normal)
+    }
+    
+    func sleepFire() {
+        sleepTimerButtonUpdateTimer?.invalidate()
+        sleepTimerButtonUpdateTimer = nil
+        
+        // Exit to Guide
+    }
+    
+    func cancelSleepTime() {
+        sleepTimerButton.setTitle("Sleep Timer: Off", for: .normal)
+        
+        if sleepTimer != nil {
+            sleepTimer?.invalidate()
+            sleepTimer = nil
+        }
+        
+        if sleepTimerButtonUpdateTimer != nil {
+            sleepTimerButtonUpdateTimer?.invalidate()
+            sleepTimerButtonUpdateTimer = nil
+        }
+    }
 
 }
 
