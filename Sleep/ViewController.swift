b6347d54b6bf2aa3ef75bca6b2b06558e1ad2e45
Stephen Woodford <stephen.woodford@thefreeosk.com>
2016-10-25 12:58:32 -0500

    Initial Commit

---
+++ Sleep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sleep
+//
+//  Created by Stephen Woodford on 10/25/16.
+//  Copyright © 2016 Stephen Woodford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
